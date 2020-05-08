ed86f6661addd5bbbe7ca1881ae2139d55f32226
’lv36900 <1984493143@qq.com>
2020-05-08 19:19:55 +0800

    周四

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 // import {reqBaseCategoryList, reqLogin} from '@/api'
+import { reqFloors } from '@/api'
 
 export default {
   name: 'App',
@@ -25,9 +26,18 @@ export default {
     // const result2 = await reqLogin('13700000000', '111111')
     // console.log('result2', result2)
 
-    console.log(this.$store.state)
-    // 测试通过异步action获取异步获取数据到vuex的state中
-    // this.$store.dispatch('getBaseCategoryList')
+    // console.log(this.$store.state)
+
+    // 测试调用mock接口对应的接口请求函数
+    // const result = await reqFloors()
+    // console.log('mock result ', result)
+
+    // 测试针对mock接口的vuex代码
+    this.$store.dispatch('getFloors')
+    this.$store.dispatch('getBanners')
+    
+    // 通过异步action获取异步获取数据到vuex的state中
+    this.$store.dispatch('getBaseCategoryList')
   },
 
   components: {
