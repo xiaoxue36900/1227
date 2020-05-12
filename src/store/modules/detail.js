133821cb56b39692388cef8577367a79f9b46304
’lv36900 <1984493143@qq.com>
2020-05-12 21:47:56 +0800

    feat 销售属性功能

--- src/store/modules/detail.js
+++ src/store/modules/detail.js
@@ -1,10 +1,10 @@
 /* 
 管理商品详情信息的vuex子模块
 */
-import { reqProduct } from "@/api";
+import { reqProduct, reqAddToCart } from "@/api";
 
 const state = {
-  detailInfo: {},
+  detailInfo: {}, // 当前商品详情信息对象
 };
 
 const mutations = {
@@ -27,6 +27,50 @@ const actions = {
       commit("RECEIVE_DETAIL_INFO", detailInfo);
     }
   },
+
+  /* 
+  添加商品到购物车的异步action
+  */
+  async addToCart({ commit }, { skuId, skuNum, callback }) {
+    const result = await reqAddToCart(skuId, skuNum);
+    if (result.code === 200) {
+      // 添加到购物车成功
+      console.log("添加到购物车成功");
+      callback(); // 成功了不传递错误信息
+      // callback({status: 0, message: '添加到购物车成功'})
+    } else {
+      // 失败
+      console.log("添加到购物车失败");
+      callback("添加到购物车失败"); // 失败了, 需要传递错误信息
+      // callback({status: 1, message: '添加到购物车失败'})
+    }
+  },
+
+  async addToCart2({ commit }, { skuId, skuNum }) {
+    const result = await reqAddToCart(skuId, skuNum);
+    if (result.code === 200) {
+      // 添加到购物车成功
+      // console.log('添加到购物车成功')
+      return ""; // async函数的promise是成功的, value是''
+    } else {
+      // 失败
+      // console.log('添加到购物车失败')
+      return "添加到购物车失败"; // async函数的promise是成功的, value为errorMsg
+    }
+  },
+
+  async addToCart3({ commit }, { skuId, skuNum }) {
+    const result = await reqAddToCart(skuId, skuNum);
+    if (result.code === 200) {
+      // 添加到购物车成功
+      // console.log('添加到购物车成功')
+      return ""; // async函数的promise是成功的
+    } else {
+      // 失败
+      // console.log('添加到购物车失败')
+      throw new Error("添加到购物车失败"); // async函数的promise就是失败的
+    }
+  },
 };
 
 const getters = {
@@ -34,10 +78,9 @@ const getters = {
   包含3级分类名称数据的对象
   */
   categoryView(state) {
-    // 初始时是undefined, 后来就有值了
-    const categoryView = state.detailInfo.categoryView;
-    // 保存返回的是一个空对象或者带数据的对象
-    return categoryView || {};
+    const categoryView = state.detailInfo.categoryView; // 初始时是undefined, 后来就有值了
+
+    return categoryView || {}; // 保存返回的是一个空对象或者带数据的对象
     // return categoryView===undefined ? {} : categoryView
   },
 
@@ -55,6 +98,14 @@ const getters = {
   skuImageList(state) {
     const skuInfo = state.detailInfo.skuInfo;
     return skuInfo ? skuInfo.skuImageList : [];
+  },
+
+  /* 
+    返回商品SPU销售属性列表
+    */
+  spuSaleAttrList(state) {
+    const spuSaleAttrList = state.detailInfo.spuSaleAttrList;
+    return spuSaleAttrList || [];
   },
 };
 
