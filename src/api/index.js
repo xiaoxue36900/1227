23f31c563c18f279b623f21b17da0130d3799cf0
’lv36900 <1984493143@qq.com>
2020-05-12 22:03:24 +0800

    fix 改bug

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@
 每个函数的返回值都是promise
 */
 
-import ajax from './ajax'
-import mockAjax from './mockAjax'
+import ajax from "./ajax";
+import mockAjax from "./mockAjax";
 
 /* 
 请求获取3级分类列表
@@ -13,9 +13,9 @@ import mockAjax from './mockAjax'
 export function reqBaseCategoryList() {
   // return ajax('/product/getBaseCategoryList')
   return ajax({
-    method: 'GET',
-    url: '/product/getBaseCategoryList'
-  })
+    method: "GET",
+    url: "/product/getBaseCategoryList",
+  });
   // return ajax.get('/product/getBaseCategoryList')
 }
 
@@ -24,28 +24,29 @@ export function reqBaseCategoryList() {
 /api/user/passport/login
 POST
 */
-export function reqLogin (mobile, password) {
+export function reqLogin(mobile, password) {
   return ajax({
-    method: 'POST',
-    url: '/user/passport/login',
-    data: {mobile, password}
-  })
+    method: "POST",
+    url: "/user/passport/login",
+    data: { mobile, password },
+  });
 
   // return ajax.post('/user/passport/login', {mobile, password})
 }
 
 /* mock接口对应的接口请求函数 */
-export const reqBanners = () => mockAjax('/banners')
-export const reqFloors = () => mockAjax('/floors')
+export const reqBanners = () => mockAjax("/banners");
+export const reqFloors = () => mockAjax("/floors");
 
 /* 
 根据搜索的条件参数对象获取商品列表数据
 */
-export const reqProductList = (searchParams) => ajax({
-  url: '/list',
-  method: 'POST',
-  data: searchParams
-})
+export const reqProductList = (searchParams) =>
+  ajax({
+    url: "/list",
+    method: "POST",
+    data: searchParams,
+  });
 // export const reqProductList = (searchParams) => ajax.post('/list', searchParams)
 
 /* 
@@ -64,6 +65,13 @@ reqProductList({
 请求获取指定id的商品信息
 /api/item/{ skuId }
 */
-export const reqProduct = (skuId) => ajax(`/item/${skuId}`) 
+export const reqProduct = (skuId) => ajax(`/item/${skuId}`);
 
 // reqProduct(6)
+
+/* 
+添加到购物车
+/api/cart/addToCart/{ skuId }/{ skuNum }
+*/
+export const reqAddToCart = (skuId, skuNum) =>
+  ajax.post(`/cart/addToCart/${skuId}/${skuNum}`);
